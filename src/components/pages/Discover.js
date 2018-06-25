9018a3f701b83ef942833e5d55e8bced35ee0563
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-06-25 16:01:37 -0500

    configured basic discover page

--- src/components/pages/Discover.js
+++ src/components/pages/Discover.js
@@ -1,18 +1,64 @@
 import React, {Component} from "react";
+import API from '../../utils/API';
+
 
 class Discover extends Component {
 
+  state = {
+    image: '',
+    match: false,
+    matchCount: 0
+  }
   
+componentDidMount() {
+  this.loadNextDog();
+}
+
+handleBtnClick = event => {
+  const btnType = event.target.attributes.getNamedItem('data-value').value;
+  const newState = {...this.state};
+
+  if(btnType === 'pick') {
+    newState.match = 1 === Math.floor(Math.random() * 5) + 1;
+
+    newState.matchCount = newState.match
+    ? newState.matchCount + 1
+    : newState.matchCount;
+  }
+  else {
+    newState.match = false;
+  }
+  this.setState(newState);
+  this.loadNextDog()
+}
+
+loadNextDog = () => {
+  API.getRandomDog()
+  .then( res => 
+    this.setState({
+      image: res.data.message
+    })
+  )
+  .catch(err => console.log(err))
+}
+
+
 
 
 render() {
-  return
-  (
-    <div>
+  return(
+      <div>
+        <h1 className="text-center">Make New Friends</h1>
+        <h3 className="text-center">
+          Thumbs up on any pups you'd like to meet!
+        </h3>
+        <img src={this.state.image} handleBtnClick={this.handleBtnClick} />
+        <h1 className="text-center">
+          Made friends with {this.state.matchCount} pups so far!
+        </h1>
+      </div>
+    );
 
-    </div>
-
-    )
 }
 
 };
